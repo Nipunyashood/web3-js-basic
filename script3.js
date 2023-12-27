5d32711b1b06723fef9ba902c3b24eea2c865d48
Nipun Yashood <152753967+Nipunyashood@users.noreply.github.com>
2023-12-27 07:49:39 -0800

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,15 @@
+function abc() {
+alert("Hello.. your successfully registered");
+
+   var congratulations = document.getElementById("y2").innerHTML="congratulations..";
+   //document.getElementById("y2").style.color="green";
+   //document.getElementById("y2").style.backgroundColor="yellow";
+   document.getElementById("y2").className="y";
+    document.getElementById("logs").className="z";
+    
+       
+}
+
+//function congratulations() {
+//document.getElementById(congratulations).className= "y3";
+//}
